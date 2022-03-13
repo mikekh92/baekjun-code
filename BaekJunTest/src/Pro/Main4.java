a1a99ba63df3cd16057b1b4a42e80c578fa67d1f
KwangHyun <mikekhkim@gmail.com>
2022-03-13 16:36:22 +0900

    20220313 8393번 백준 문제 코드

--- BaekJunTest/src/Pro/Main4.java
+++ BaekJunTest/src/Pro/Main4.java
@@ -1,5 +1,18 @@
 package Pro;
 
-public class Main4 {
+import java.util.Scanner;
 
+public class Main4 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum=0;
+		for( int i =0; i<n+1; i++  ) {
+			sum+=i;
+		}
+		sc.close();
+		System.out.println(sum);
+	}
 }
