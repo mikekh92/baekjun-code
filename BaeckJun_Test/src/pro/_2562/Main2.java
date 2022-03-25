657ef7d7191436deedd3e0d9477aaea05395efeb
user1 <user1@DESKTOP-5C6ULF0>
2022-03-25 13:31:19 +0900

    20220325 백준 코드 2562번

---
+++ BaeckJun_Test/src/pro/_2562/Main2.java
@@ -0,0 +1,30 @@
+package pro._2562;
+
+
+		import java.io.BufferedReader;
+		import java.io.InputStreamReader;
+		import java.io.IOException;
+		 
+		public class Main2 {
+			public static void main(String[] args) throws IOException {
+				
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+				int max = 0;
+				int index = 0;
+				for(int i = 0 ; i < 9 ; i++) {
+		 
+					int val = Integer.parseInt(br.readLine());
+					
+					if(val > max) {
+						max = val;
+						index = i+1;
+					}
+				}
+		 
+				System.out.println(max);
+				System.out.println(index);
+			}
+		
+	}
+
