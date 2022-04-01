a115469efccb2c8558e41e96d4c2997f89e2ad0b
KwangHyun <mikekhkim@gmail.com>
2022-04-01 12:07:38 +0900

    20220401 백준 자바 문제

---
+++ BaekJunTest/src/Pro/_11654/Main.java
@@ -0,0 +1,17 @@
+package Pro._11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int ch = sc.next().charAt(0);
+		
+		sc.close();
+		System.out.println(ch);
+	}
+
+}
