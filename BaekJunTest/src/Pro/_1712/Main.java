67e0cfea96b64aba7d2305357e236aef2c661182
KwangHyun <mikekhkim@gmail.com>
2022-04-05 21:59:51 +0900

    20220405 백준 자바 문제

---
+++ BaekJunTest/src/Pro/_1712/Main.java
@@ -0,0 +1,22 @@
+package Pro._1712;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if( c<=b) {
+				System.out.println( -1 );
+			}else {
+				System.out.println((a/(c-b))+1);
+			}sc.close();
+	}
+
+}
