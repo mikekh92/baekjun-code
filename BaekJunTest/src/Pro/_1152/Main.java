32cb39ab80922680257a9068933b00d2623ef0ce
KwangHyun <mikekhkim@gmail.com>
2022-04-29 22:41:36 +0900

    20220429 백준 자바 1152문제

---
+++ BaekJunTest/src/Pro/_1152/Main.java
@@ -0,0 +1,20 @@
+package Pro._1152;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		sc.close();
+		
+		StringTokenizer st = new StringTokenizer(s," ");
+		System.out.println(st.countTokens());
+		
+	}
+
+}
