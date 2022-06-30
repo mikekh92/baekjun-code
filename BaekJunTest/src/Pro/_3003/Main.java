17db90366973b4c7b7c806a1677fa5f5e028ee06
mikekh92 <mikekhkim@gmail.com>
2022-06-30 23:57:05 +0900

    20220630 백준자바 문제 3003,4101,4999

---
+++ BaekJunTest/src/Pro/_3003/Main.java
@@ -0,0 +1,17 @@
+package Pro._3003;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[6];
+		int b[] = {1,1,2,2,2,8};
+		for(int i=0; i<a.length; i++) {
+			a[i]=sc.nextInt();
+		}
+		for(int i =0; i<a.length; i++) {
+			System.out.print(b[i]-a[i]+ " ");
+		}
+	}
+}
---
+++ BaekJunTest/src/Pro/_4999/Main.java
@@ -0,0 +1,18 @@
+package Pro._4999;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		String b = sc.next();
+		
+		if (a.length() >= b.length()) {
+			System.out.println("go");
+		}else {
+			System.out.println("no");
+		}
+	}
+}
